54f30d864d3457fb8b5dbcc82c62021f61915acd
Eric Sun <eric.sun.ecs92@yale.edu>
2023-07-21 16:25:42 -0400

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./styles.module.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [userPrompt, setUserPrompt] = useState("");
+  const [sqlQuery, setSqlQuery] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("submitted form")
+    // const query = await generateQuery();
+    // setSqlQuery(query);
+  };
+
+  // const generateQuery = async () => {
+  //   const response = await fetch("http://localhost:3002/generate", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ queryDescription: userPrompt }),
+  //   });
+
+  //   const data = await response.json();
+  //   return data.sqlQuery.trim();
+  // };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.main}>
+      <h3>Generate SQL</h3>
+      <form onSubmit={() => onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          value={userPrompt}
+          onChange={(e) => setUserPrompt(e.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+      </form>
+      <pre>{sqlQuery}</pre>
+    </main>
+  );
 }
-
-export default App
